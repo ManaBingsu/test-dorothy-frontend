8ecea611370100cb8145891c01100b59ad870d5f
Build Machine <SouthPAW Games@DESKTOP-6ISBFO1>
2020-09-15 14:05:57 +0900

    feat : Login form

--- client/src/routes/Auth.js
+++ client/src/routes/Auth.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
     const onChange = (event) => {
         const {target: {name, value}} = event;
         if (name === "email"){
@@ -11,15 +12,21 @@ const Auth = () => {
             setPassword(value);
         }
     };
+    // 기본 행위가 실행되는 것을 원치 않음
     const onSubmit = (event) => {
         event.preventDefault();
+        if (newAccount) {
+            // create account
+        } else {
+            // log in
+        }
     };
     return (
         <div>
         <form onSubmit={onSubmit}>
             <input 
                 name="email" 
-                type="text" 
+                type="email" 
                 placeholder="Email" 
                 required value={email} 
                 onChange={onChange}
@@ -31,7 +38,7 @@ const Auth = () => {
                 required value={password} 
                 onChange={onChange}
                 />
-            <input type="submit" value="Log In" />
+            <input type="submit" value={newAccount ? "Create Account" : "Log In"}/>
         </form>
         </div> 
     )
